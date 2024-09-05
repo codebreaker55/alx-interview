4a1817c3f2f387cb1b3b3388e3437234d9dfceea
belal <belalismail252@gmail.com>
2024-09-05 11:40:37 +0300

    semistandard

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const req = require('request'); // Using req as the alias for request
+const movieId = process.argv[2];
+const urlApi = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+req(urlApi, async (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const charactersArray = JSON.parse(body).characters;
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      req(character, (err, res, body) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
+  }
+});
