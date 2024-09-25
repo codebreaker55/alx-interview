24c6d632364b303f5ec1d472191c3183c3167348
belal <belalismail252@gmail.com>
2024-09-26 01:13:02 +0300

    perimeter island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,19 +10,23 @@ def island_perimeter(grid):
     of the island described in grid
     """
     perimeter = 0
+
+    # validate input is a list of lists
     if type(grid) != list:
         return 0
-    n = len(grid)
-    for j, row in enumerate(grid):
-        m = len(row)
-        for k, cell in enumerate(row):
+    n = len(grid)  # the number of rows in the grid
+    for r, row in enumerate(grid):
+        m = len(row)  # the number of columns in the current row
+        for c, cell in enumerate(row):
             if cell == 0:
                 continue
+            # see edges to determine how many sides contribute with perimeter
             edges = (
-                j == 0 or (len(grid[j - 1]) > k and grid[j - 1][k] == 0),
-                k == m - 1 or (m > k + 1 and row[k + 1] == 0),
-                j == n - 1 or (len(grid[j + 1]) > k and grid[j + 1][k] == 0),
-                k == 0 or row[k - 1] == 0,
+                r == 0 or (len(grid[r - 1]) > c and grid[r - 1][c] == 0),
+                c == m - 1 or (m > c + 1 and row[c + 1] == 0),
+                r == n - 1 or (len(grid[r + 1]) > c and grid[r + 1][c] == 0),
+                c == 0 or row[c - 1] == 0,
             )
+            # add the number of edges that are part of the perimeter
             perimeter += sum(edges)
     return perimeter
